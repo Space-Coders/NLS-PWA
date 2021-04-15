3384a903bb75d2486a298e8a0261bc6c527d396e
DevDHera <devdhera@gmail.com>
2021-04-15 19:40:27 +0530

    feat(data): :sparkles: Base setup for data module

---
+++ src/app/data/data.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DataComponent } from './data.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [DataComponent]
+})
+export class DataModule { }
