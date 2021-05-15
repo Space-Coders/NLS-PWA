30cec48d979ba9097bf5b3050dceb27adc8fc9cd
DevDHera <devdhera@gmail.com>
2021-05-15 12:23:26 +0530

    feat(dashboard): :sparkles: Adds base dashboard

--- src/app/module/dashboard/dashboard.module.ts
+++ src/app/module/dashboard/dashboard.module.ts
@@ -1,10 +1,22 @@
+// core modules
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+// shared modules
+import { SharedModule } from '@shared/shared.module';
+
+// resolvers
+
+// components
+import { DashboardComponent } from './page/dashboard/dashboard.component';
+import { NotFoundComponent } from './page/not-found/not-found.component';
+
+// routing
+import { DashboardRoutingModule } from './dashboard.routing';
+
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: []
+    imports: [CommonModule, SharedModule, DashboardRoutingModule],
+    declarations: [DashboardComponent, NotFoundComponent],
+    providers: [],
 })
-export class DashboardModule { }
+export class DashboardModule {}
